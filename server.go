a6b6cfb7d1451d18c3b0442748c4ba6037099484
Uju-Data <ujuofiani@gmail.com>
2026-03-10 15:53:31 +0100

    Update server.go

--- server.go
+++ server.go
@@ -1,19 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"os"
 )
 
-func startDummyServer() {
+func startServer() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "10000"
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Helsa bridge running")
+		w.Write([]byte("Helsa bridge running"))
 	})
 
 	go http.ListenAndServe(":"+port, nil)
